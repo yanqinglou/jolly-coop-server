e41f46bd2653ffc6f2ab85c7255a54e3963a7af8
ber <yanqinglou@outlook.com>
2023-03-28 15:11:51 -0700

    update

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
 
-app.use((req, res, next) => {
-    res.header({"Access-Control-Allow-Origin": "*"});
-    next();
-  }) 
+// app.use((req, res, next) => {
+//     res.header({"Access-Control-Allow-Origin": "*"});
+//     next();
+//   }) 
 
 app.use('/',allRoutes);
 
